cfcfc83992132929e6e06d038bf55823c34b2446
yongkang <zhangyongkang@lanxiniu.com>
2016-10-16 18:09:30 +0800

    practice

--- server/main.go
+++ server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,19 +11,31 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+func openLog() {
+	flag.Lookup("logtostderr").Value.Set("true")
+	flag.Parse()
+}
 func main() {
+	openLog()
+
 	router := httprouter.New()
-	router.GET("/hi", Hi)
+
+	router.GET("/", welcome)
+	router.GET("/andy/:name", hi)
+	router.GET("/redis", checkRedis)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func Hi(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprintf(w, "hi, this is %s", "httprouter")
+func welcome(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprintf(w, "welcome, this is powered by %s", "httprouter")
 }
 
-func tryRedis2() {
-	// conn := myredis.NewConn()
+func hi(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello %s", ps.ByName("name"))
+}
+
+func checkRedis(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	conn, err := redis.Dial("tcp", "redis:6379")
 	if err != nil {
 		panic(err)
@@ -33,9 +46,10 @@ func tryRedis2() {
 		glog.Error(err)
 	}
 	glog.Infoln("v: ", v)
+
 	v, err = redis.String(conn.Do("GET", "name"))
 	if err != nil {
 		glog.Error(err)
 	}
-	glog.Info("value by get: ", v)
+	fmt.Fprintf(w, "check redis, the value got is %s", v)
 }
